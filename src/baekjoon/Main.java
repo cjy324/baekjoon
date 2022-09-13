fe6ffd3141c6c057fe344415553b59248828d863
cjy324 <juy32400@gmail.com>
2022-09-13 09:38:22 +0900

    14248번 점프점프 정답

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,82 +1,117 @@
 package baekjoon;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int n;
+    static int check[]; // 방문 완료
+    static int bridge[]; // 돌다리
+    static int dir[] = {1, -1}; // 왼쪽 방향, 오른쪽 방향
+    static int cnt = 1; // 시작 지점도 방문한 것이니 1로 초기화
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		// 1213번 
-		// 팰린드롬 만들기
-		// https://www.acmicpc.net/problem/1213
+		// 14248번 
+		// 점프 점프
+		// https://www.acmicpc.net/problem/14248
 		
 		// 문제
 		/*
-			임한수와 임문빈은 서로 사랑하는 사이이다.
-			임한수는 세상에서 팰린드롬인 문자열을 너무 좋아하기 때문에, 둘의 백일을 기념해서 임문빈은 팰린드롬을 선물해주려고 한다.
-			임문빈은 임한수의 영어 이름으로 팰린드롬을 만들려고 하는데, 임한수의 영어 이름의 알파벳 순서를 적절히 바꿔서 팰린드롬을 만들려고 한다.
+			영우는 개구리다 개굴개굴개굴
+
+			영우는 지금 n개의 돌이 일렬로 놓여있는 돌다리에 있다. 
+			그리고 돌다리의 돌에는 숫자가 하나씩 적혀있다. 
+			영우는 이 숫자가 적혀있는 만큼 왼쪽이나 오른쪽으로 점프할 수 있는데, 이때 돌다리 밖으로 나갈 수는 없다.
 			
-			임문빈을 도와 임한수의 영어 이름을 팰린드롬으로 바꾸는 프로그램을 작성하시오.
+			영우는 이 돌다리에서 자기가 방문 가능한 돌들의 개수를 알고 싶어한다. 
+			방문 가능하다는 것은 현재위치에서 다른 돌을 적절히 밟아 해당하는 위치로 이동이 가능하다는 뜻이다.
+			
+			현재 위치가 주어졌을 때, 영우가 방문 가능한 돌들의 개수를 출력하시오.
 		*/
 		
 		// 입력
 		/*
-			첫째 줄에 임한수의 영어 이름이 있다. 알파벳 대문자로만 된 최대 50글자이다.
+			첫 번째 줄에는 돌다리의 돌 개수 n이 주어진다.(1≤n≤100,000) 
+			돌의 번호는 왼쪽부터 1번에서 n번이다. 
+			다음 줄에는 그 위치에서 점프할 수 있는 거리 Ai가 주어진다.(1≤Ai≤100,000)
+
+			다음 줄에는 출발점 s가 주어진다.(1≤s≤n)
 		*/
 		
 		// 출력
 		/*
-		 	첫째 줄에 문제의 정답을 출력한다. 
-		 	만약 불가능할 때는 "I'm Sorry Hansoo"를 출력한다. 정답이 여러 개일 경우에는 사전순으로 앞서는 것을 출력한다.
+		 	영우가 방문 가능한 돌들의 개수를 출력하시오.
 		*/
 			
 		// 예제 입력
 		/* 
-			AABB
-			
-			ABACABA
-			
-			ABCD
+			5
+			1 4 2 2 1
+			3
 		*/
 		
 		// 답
 		/* 
-		 	ABBA
-		 	
-		 	AABCBAA
-		 	
-		 	I'm Sorry Hansoo
+		 	5
 		*/
-
-		Scanner sc = new Scanner(System.in);
-		
-		// 팰린드롬??? 역순으로 읽어도 같은 말 ex) eye, madam
-		// 정답이 여러 개일 경우에는 사전순으로 앞서는 것을 출력
-		// 불가능할 때는 "I'm Sorry Hansoo"를 출력
-		
-		String name = sc.nextLine();
-		char[] nameArr = name.toCharArray();
-		Arrays.sort(nameArr);  // 알파벳 순으로 정렬
-		
-		if(nameArr.length%2 == 0) {		// 1. 짝수일 경우  // aaaabbbbcccc
-			
-		} else {		// 2. 홀수일 경우    // aaabccc
-			
-		}
 		
 		
-		// 정답 예시
-		// https://developer-ellen.tistory.com/91
 		
+		// POINT
+		// 숫자가 적혀있는 만큼 왼쪽이나 오른쪽으로 점프할 수 있는데, 이때 돌다리 밖으로 나갈 수는 없다.
+		// 자기가 방문 가능한 돌들의 개수???
 		
+		// 돌의 번호는 왼쪽부터 1번에서 n번
+		// 다음 줄에는 그 위치에서 점프할 수 있는 거리 Ai
+		// 다음 줄에는 출발점 s
+		
+		// 현재 위치가 주어졌을 때, 영우가 방문 가능한 돌들의 개수를 출력
 		
 
-		
-		
-		
-		
-		sc.close();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        bridge = new int[n]; 
+        check = new int[n]; 
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        // 돌다리 담기
+        for (int i = 0; i < n; i++) {
+            bridge[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int s = Integer.parseInt(br.readLine()); // 시작지점
+        System.out.println(bfs(s-1)); // 배열 인덱스는 s-1
+
 	}
+	
+	public static int bfs(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
+        check[node] = 1; // 방문한 곳 체크
+        
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            int jump = bridge[node];
+            for (int d : dir) {
+                int direction = (d * jump) + node;
+                if (direction >= 0 && direction < n && check[direction] == 0) {
+                    check[direction] = 1;
+                    queue.offer(direction);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+	
+	
+	// 정답: https://connie.tistory.com/m/16
 
 }
